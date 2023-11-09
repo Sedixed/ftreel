d6d7b0a073848babc16613fa16c359aaa1692f5c
Hitowa <alexandre.merre.pro@gmail.com>
2023-11-09 19:23:29 +0100

    feat: Deleted unnecessary files and added database connection string and context

--- ftreel/Program.cs
+++ ftreel/Program.cs
@@ -1,12 +1,19 @@
+using ftreel.DATA;
+using Microsoft.EntityFrameworkCore;
+
 internal class Program
 {
-
     private static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
+        builder.Services.AddDbContext<AppDBContext>(options =>
+        {
+            var a = builder.Configuration.GetConnectionString("Database");
+            options.UseSqlServer(builder.Configuration.GetConnectionString("Database"));
+        });
 
+        // Add services to the container.
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
